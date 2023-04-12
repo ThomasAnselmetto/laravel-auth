189b9e932c7a404980f9f9805b8c2fc1198525da
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-12 16:47:54 +0200

    create msrc

--- app/Http/Controllers/Admin/Homecontroller.php
+++ app/Http/Controllers/Admin/Homecontroller.php
@@ -9,6 +9,6 @@ use Illuminate\Http\Request;
 class Homecontroller extends Controller
 {
    public function index(){
-        return view('dashboard');
+        return view('admin.home');
     }
 }
--- app/Http/Controllers/Guest/Homecontroller.php
+++ app/Http/Controllers/Guest/Homecontroller.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class Homecontroller extends Controller
 {
    public function index() {
-        return view('welcome');
+        return view('guest.home');
     }
 }
