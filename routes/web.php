d3d0ecacde006387ef251e8fe73b4bc7703b71ef
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-12 09:30:50 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +9,29 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')
+// tutte le rotte di questo gruppo hanno come prefisso profile e gli do prefix()
+    ->prefix('profile')
+    // tutti i nomi delle rotte di questo gruppo iniziano con profile quindi do 
+    ->name('profile.')
+    // group va sempre per ultimo
+    ->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
+});
+
+require __DIR__.'/auth.php';
